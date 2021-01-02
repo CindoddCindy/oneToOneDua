1120c56749269dd77905d1a23a0bfd204da09a81
CindoddCindy <cindodcindy@gmail.com>
2021-01-02 22:12:33 +0700

    cuma tambah package

---
+++ onetoonedua/src/main/java/oneToOneDua/onetoonedua/OnetooneduaApplication.java
@@ -0,0 +1,13 @@
+package oneToOneDua.onetoonedua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetooneduaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnetooneduaApplication.class, args);
+	}
+
+}
---
+++ onetoonedua/src/test/java/oneToOneDua/onetoonedua/OnetooneduaApplicationTests.java
@@ -0,0 +1,13 @@
+package oneToOneDua.onetoonedua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetooneduaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
