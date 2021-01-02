e5263603541f0f316714cdb445478e1d88f31154
CindoddCindy <cindodcindy@gmail.com>
2021-01-02 22:29:56 +0700

    tambah kelas kela

---
+++ onetoonedua/src/main/java/oneToOneDua/onetoonedua/repository/OrganizationRepository.java
@@ -0,0 +1,10 @@
+package oneToOneDua.onetoonedua.repository;
+
+
+import oneToOneDua.onetoonedua.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository  extends JpaRepository<Organization, Long> {
+}
